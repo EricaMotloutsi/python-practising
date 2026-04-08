69418e09e502a593d0e44607dcbaaf3fbc82b404
EricaMotloutsi <ericamotloutsi@gmail.com>
2026-04-08 20:25:48 +0000

    While loops

---
+++ Extra_practice.A/ranges.py
@@ -0,0 +1,12 @@
+# ranges
+for participant_id in range(1, 55):
+    print(f'Assigning ID to participant #{participant_id}')
+
+numbers = list(range(1, 10, 2))
+print(numbers)
+
+for _ in range(5):
+    print('Analyzing Data')
+
+print('Hello')
+    
---
+++ while_loops.py
@@ -0,0 +1,7 @@
+#While loops
+
+temperature = 70
+while temperature < 80:
+    print(f'The current temperature is {temperature}f')
+    temperature +=1
+    
